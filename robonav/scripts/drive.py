eb41e23336c4bca28144e44aab7ea23fc05f1d4d
Paul Krusell <paul.krusell@students.olin.edu>
2017-05-05 03:57:59 -0400

    tweaked parameters and cleaned/styled the code

--- robonav/scripts/drive.py
+++ robonav/scripts/drive.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # the neato drives to given point from given coordinate.
 
+# IMPORTS
 from map_reading import MapReadingNode
 import rospy
 from geometry_msgs.msg import Vector3, Twist, Point, Pose
@@ -16,19 +17,23 @@ class drive_node(object):
     def __init__(self, start, destination, startangle):
         """ Initialize node"""
         self.initialize = False
-        map_reading = MapReadingNode()    
+        
         self.r = rospy.Rate(5)
+
         self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.time = rospy.Time.now()
-        self.startangle = 0
+
+        # Initial values
+        self.startangle = startangle
         self.current_pos = start
-        self.current_angle = 0
         self.destination = destination
         self.destination_reached = False    
 
-        self.turnspeed = math.pi/16 #rad/s
-        self.drive_speed = .2 #m/s    
+        # Speeds
+        self.turnspeed = math.pi/16 
+        self.drive_speed = .1   
 
+        # Start not moving
         self.angular_twist = Vector3(0,0,0)
         self.linear_twist = Vector3(0,0,0)
 
@@ -54,20 +59,19 @@ class drive_node(object):
             else:
                 target_angle = np.arctan(xdiff/ydiff) # In radians
             angle = target_angle - self.startangle
-            if angle > 0:
+            if angle > 0 and angle < 3.14:
                 print "destination is to the right"
-                self.turnspeed = -1 * abs(self.turnspeed)
+                self.turnspeed = 1* abs(self.turnspeed)
             else:
                 print "destination is to the left"
-                self.turnspeed = abs(self.turnspeed)
+                self.turnspeed = -1* abs(self.turnspeed)
 
             print "destination " + str(angle) + " radians away"
             turn_time = abs(angle/self.turnspeed) #radians/(radians/second) = seconds
-            #turn_time = 4
-            #turn_speed = 2*math.pi/8
+           
             self.linear_twist  = Vector3(0, 0, 0)
             self.time = rospy.Time.now()
-            #self.turnspeed = 1
+
             print "turn time = " + str(turn_time)
             while(rospy.Time.now() - self.time < rospy.Duration(turn_time)):
                 self.angular_twist = Vector3(0, 0, self.turnspeed)
@@ -80,11 +84,10 @@ class drive_node(object):
 
     def drive(self):
         """ The function to drive forward towards the destination. """
-        # print "driving forward"
         xdiff = self.destination[0] - self.current_pos[0] 
         ydiff = self.destination[1] - self.current_pos[1]
 
-        distance = math.sqrt(xdiff**2 + ydiff**2) # meters
+        distance = (math.sqrt(xdiff**2 + ydiff**2)) # meters
         print ("distance = ", distance)
 
         drive_time = abs(distance/self.drive_speed)
@@ -92,9 +95,8 @@ class drive_node(object):
         while(rospy.Time.now() - self.time < rospy.Duration(drive_time)):
                 self.linear_twist = Vector3(self.drive_speed, 0, 0)
                 self.publish_twist()
-                # print "driving for " + str(drive_time) + " seconds"
         self.destination_reached = True
-        # print "done driving forward"
+        print "done driving forward"
     def publish_twist(self):
         self.publisher.publish(Twist(linear=self.linear_twist, angular=self.angular_twist))
       
@@ -109,15 +111,11 @@ class drive_node(object):
                 self.drive()
             else:
                 self.stop()
+                return
         self.stop()
 
 
 if __name__ == '__main__':
-    
-    map_reading = MapReadingNode()    
-    node_values = map_reading.grid_map.occupancy_grid
-
-    print("completed reading map")
     rospy.init_node('go_to_test')
     time = rospy.Time.now()
     node = drive_node((0,0),(1,1), 0)
--- robonav/scripts/nav.py
+++ robonav/scripts/nav.py
@@ -1,85 +1,97 @@
 #!/usr/bin/env python
 # Use a_star algorithm to go to point on map from point on map
+
+# IMPORTS
+
+# ROS
 import rospy
-from map_reading import MapReadingNode
-from a_star import search_algorithm
-print "HELLO"
-from a_star import graph_node
-# from drive import drive_node
 from std_msgs.msg import Header, String
 from sensor_msgs.msg import LaserScan, PointCloud
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, PoseArray, Pose, Point, Quaternion
-from tf import TransformListener
-from tf import TransformBroadcaster
+
+# Transforms
+import tf
 from tf.transformations import euler_from_quaternion, rotation_matrix, quaternion_from_matrix, quaternion_from_euler
-import time
-import rospy
-import sys
+
+# General
 import matplotlib.pyplot as plt
 import numpy as np
-import copy
+
+# Other classes
+from map_reading import MapReadingNode
 from drive import drive_node
-import random
-
-
+from a_star import search_algorithm
+from a_star import graph_node
 
 
 class neato_navigation(object):
-	def __init__(self, dest=(40,40)): # , start, dest, startangleamcl_pose 
+	""" The main neato navigation class: bringing it all together"""
+	def __init__(self, dest=(40,40)):
 		self.initialized = False
+
+		# Goal Destination
+		self.dest = dest
+
+		# Map
 		self.map_reading = MapReadingNode()	
 		self.nav_map = self.map_reading.grid_map
 		self.nav_map_info = self.map_reading.info
-		print self.nav_map_info
-		# self.start = start
-		self.dest = dest
-		self.node_values = self.nav_map.occupancy_grid
+		
+		# Later used for a_star class
 		self.a_star = None
+
+		# Lists for generated waypoints
 		self.waypoints = []
 		self.waypoints_meters = []
 
+		# Pos from amcl builtin localizer
 		self.x = 0
 		self.y = 0
 
+		# Pos converted to visualization graph coordinate frame
 		self.vizx = 0
 		self.vizy = 0
 
-		self.quaternion = []
-		self.angle = 0
-
-		self.updates = 0
-		self.viz_count = 0
-
+		# Pos in pixels
 		self.xp = 0
 		self.yp = 0
 
-		self.path_found = False
+		# Pos converted to meters
+		self.xm = 0 
+		self.ym = 0 
 
-		self.xm = 0 # x position in  meters
-		self.ym = 0 # y position in meters
-
+		# Angle from localizer
+		self.quaternion = []
+		self.angle = 0
+		
+		# Subscribers
 		rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.update_pos)
-
 		rospy.Subscriber("initialpose", PoseWithCovarianceStamped, self.update_initial)
 
 		self.r = rospy.Rate(15)
-		print('this is doing something')
 
-		self.tf = TransformListener
+		print("Please choose an initial pose")
 
-	def update_initial(self, msg):
-		self.x = msg.pose.pose.position.x
-		self.y = msg.pose.pose.position.y
 
-		self.quaternion = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
-						msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
-		self.angle = euler_from_quaternion(self.quaternion)
-		print('updating the initial')
-		self.initialized = True
+	def update_viz(self):
+		"""
+		Color points that the neato has been visited on the matrix for visualization plot.
+		"""
+		if not (self.initialized):
+
+			return
+		if not (self.a_star):
+			return
+		self.vizx = (self.xm/self.nav_map_info.resolution)
+		self.vizy = (self.ym/self.nav_map_info.resolution)
+		self.a_star.viz_grid[int(self.vizx), int(self.vizy)] = 9
+		#self.visualize() #Uncomment for RC path following w/ teleop
+
+		return
 
 	def visualize(self, close=False):
 		"""
-		Visualises neato locatoin
+		Visualises neato location over path generated from A* 
 		"""
 		viz_grid = self.a_star.viz_grid
 		plt.clf()
@@ -90,173 +102,119 @@ class neato_navigation(object):
 		plt.pause(.01)
 		plt.show(close)
 
+	def update_initial(self, msg):
+		"""
+		Updates the initial pose: when 2D pose estimate is made in RVIZ
+		"""
+		self.x = msg.pose.pose.position.x
+		self.y = msg.pose.pose.position.y
+
+		self.quaternion = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
+						msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
+		self.angle = euler_from_quaternion(self.quaternion)
+		print('updating the initial')
+		self.initialized = True
+
+	
+
 	def update_pos(self, msg):
+		"""
+		Callback function to update the position when position in localizer.
+		"""
 		self.x = msg.pose.pose.position.x
 		self.y = msg.pose.pose.position.y
 		self.quaternion = [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
 						msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
 		self.get_position_meters()
 		self.angle = euler_from_quaternion(self.quaternion)[2]
-		print('angle', self.angle)
-		# if self.a_star:
-		self.update_viz()
-
-
-	def update_viz(self):
-		if not (self.initialized):
-
-			return
-		if not (self.a_star):
-			return
-		# print('visualizing')
-		self.vizx = (self.xm/self.nav_map_info.resolution)
-		self.vizy = (self.ym/self.nav_map_info.resolution)
-		self.a_star.viz_grid[int(self.vizx), int(self.vizy)] = 9
-
-		# print("POSITION :", self.xm, "meters ",self.ym, "meters" )
-		# print("UPDATING VIZ POS TO :", int(self.vizy), int(self.vizx))
-			#self.visualize()
-		# print('initializing', self.xm, self.ym)
-
-
-
-		# print('msg frame', msg.header.frame_id)
-		# t = self.tf.getLatestCommonTime(msg.header.frame_id, "map")
-		# position, quaternion = self.tf.lookupTransform(msg.header.frame_id, "map", t)
-		# print('transform stuff')
-		# print position, quaternion
-
-
-		return
-
+		
+		self.update_viz() 
 
 
 	def get_position_meters(self):
+		"""
+		Convert position from raw particle filter input to position in meters.
+		"""
 		if not (self.initialized):
 
 			return
-		self.xm = self.x - self.nav_map_info.origin.position.x # mult by res? 
+		self.xm = self.x - self.nav_map_info.origin.position.x  
 		self.ym = self.y - self.nav_map_info.origin.position.y 
-		print('positions', self.xm, self.ym)
 
 	def get_position_pixels(self):
+		""" Get current location pixel coordinates. """
 		self.xp = int((-self.nav_map_info.origin.position.x + self.x) / self.nav_map_info.resolution)
 		self.yp = int((-self.nav_map_info.origin.position.y + self.y) / self.nav_map_info.resolution)
 
 	def get_pixels_meters(self, x, y):
+		""" Convert any x and y coordinates (in meters) to pixels. """
 		xp = x / self.nav_map_info.resolution
 		yp = y / self.nav_map_info.resolution
 		return xp, yp
 		
 	def get_waypoints_meters(self):
+		""" Converts all waypoints in waypoints list to units of meters """
 		if not (self.a_star):
 			return
 		for waypoint in self.a_star.waypoint_list:
-			# print('non metered waypoint', waypoint)
-			waypoint_meter_x = waypoint[1] * self.nav_map_info.resolution - self.nav_map_info.origin.position.x * self.nav_map_info.resolution
-			waypoint_meter_y = waypoint[0] * self.nav_map_info.resolution - self.nav_map_info.origin.position.y * self.nav_map_info.resolution
+			waypoint_meter_x = waypoint[1] * self.nav_map_info.resolution + self.nav_map_info.origin.position.x * self.nav_map_info.resolution
+			waypoint_meter_y = waypoint[0] * self.nav_map_info.resolution + self.nav_map_info.origin.position.y * self.nav_map_info.resolution
 			waypoint_meter = (waypoint_meter_x, waypoint_meter_y)
 			self.waypoints_meters.append(waypoint_meter)
-			print('metered waypoint', waypoint_meter)
-
-	def check_location(self, expected):
-		pass
-		# if location != expected:
-		# self.drive_node(location, expected, )
-		# self.drive_node.run()
 
 	def driver(self, destination):
-		"""drives to a destination"""
+		"""drives to a destination using drive_node"""
 		if not (self.initialized):
-			# print('not initialized yet')
 			return
-		# print('driving')
 		if not (self.a_star):
 			return
+
 		d = drive_node((self.ym, self.xm), destination, self.angle)
 		d.run()
 
 	def a_star_creation(self, destination):
+		""" Finds a path using a_star. """ 
 		if not (self.initialized):
 			return
 
 		if (self.a_star):
 			return
-		print('doing a_star stuff')
-		# x's and y's are flipped for this part
+		# initialize search algorithm
 		self.a_star = search_algorithm((int(self.yp), int(self.xp)), destination)
-		# self.a_star = search_algorithm((19,60), (13,60))
+		# find path
 		self.a_star.find_path()
-		self.path_found = True
-
-
+	
 
 	def runner(self):
+		""" Main run loop """
 		r = rospy.Rate(0.5)
-		# while not rospy.is_shutdown():
-		# 	if self.initialized:
-		# 		print('in running loop')
-		# 		print('self.xm, self.ym', self.xm, self.ym)
-		# 		if (self.xm, self.ym) != (None,None):
-		# 			print('in if statement')
-		# 			self.xm = int(self.xm)
-		# 			self.ym = int(self.ym)
-		# 			print('coordinates', self.xm, self.ym)
-		# 			print('types', type(self.xm), type(self.ym), type(self.dest[0]), type(self.dest[1]))
-		# 			self.a_star = search_algorithm((self.xm, self.ym), (self.dest))
-		# 			self.a_star.find_path()
-		# 			print('find path completed')
-		# 			self.get_waypoints_meters()
+		destination_reached = False
 
-		# 		print('waypoints in meters', self.waypoints_meters)
-		# 	else:
-		# 		print('stuck', self.node_values[self.vizy, self.vizx])
-		# 		r.sleep()
-		# 	while len(self.waypoints_meters) != 0:
-		# 		print('in second while loop')
-		# 		target = self.waypoints_meters.pop(0)
-		# 		self.driver(target)
-
-		# 		self.visualize()
-		# 		r.sleep()
-		# r.sleep()
-
-		# def run(self):
-		# 	for i in len(self.waypoints-1):
-		# 		if i = 0:
-		#     		self.drive_node(start, waypoints[i])
-		#     		self.drive_node.run()
-		#     	else:
-		#     		self.drive_node(waypoints[i-1], waypoints[i])
-		#     	self.drive_node.run()
-		driven_to = False
-		while not (rospy.is_shutdown()):
+		while not (rospy.is_shutdown()) and destination_reached == False:
 			self.get_position_meters()
 			self.get_position_pixels()
 			dx, dy = self.get_pixels_meters(self.xm + 1, self.ym + 1)
-			#test_dest = (13, 60)
-			test_dest = (40, 52)
+			test_dest = (80, 80)
 			self.a_star_creation(test_dest)
 			self.get_waypoints_meters()
-			# print('driving')
-			# print('destination', (self.xm + 1, self.ym + 1))
-			# print('done driving')
-			if len(self.waypoints_meters)>0 and not driven_to:
-				point = self.waypoints_meters[0]
-				print ("POINT", point)
-				# print(point)
-				self.driver((point[1],point[0]))
-				driven_to = True
+			
 
-			#self.driver((self.xm - 3, self.ym - 3))
-			# if (self.path_found):
-			# 	print('path found')
-			# 	# point = self.waypoints_meters[0]
-			# 	print('waypoint', point)
-			# 	# self.driver(point)
-			# # print('driving to', test_dest, (self.xm, self.ym))
-			# 	# rospy.sleep(1)
-			# 	print('if statemenet in runner')
+			# Comment this block for RC path following w/ teleop 
+			if not (destination_reached):
+				
+				if len(self.waypoints_meters)>0:
+					for point in self.waypoints_meters:
+						print('waypoint', point)
+						print('current pos', self.ym, self.xm)
+						print ("POINT", point)
+						self.driver((point[0],point[1]))
+						print ("reached point ", point)
+						self.visualize()
+						rospy.sleep(2)
+					destination_reached = True
+					print("destination_reached: ", destination_reached)
+			# End Comment for RC path following w/ teleop
+			
 			r.sleep
 
 
